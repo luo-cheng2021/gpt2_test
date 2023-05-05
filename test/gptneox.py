1d1d559040a9fdf4692e03d648d019ebc9ad7e4e
Luo Cheng <cheng.luo@intel.com>
2023-05-05 08:46:30 +0300

    use pytorch as attn testcase reference

--- test/gptneox.py
+++ test/gptneox.py
@@ -305,8 +305,207 @@ def gen_ov_gpt_bf16(qkvs, beam_idxs, attn_masks):
         results.append(np.array(r.data))
     return results
 
+##############################################################################33
+# int8 section
+class GPTNeoXAttentionInt8(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.num_attention_heads = config.num_attention_heads
+        self.hidden_size = config.hidden_size
+        self.head_size = self.hidden_size // self.num_attention_heads
+        self.rotary_ndims = int(self.head_size * config.rotary_pct)
+        max_positions = config.max_position_embeddings
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones((max_positions, max_positions), dtype=torch.bool)).view(
+                1, 1, max_positions, max_positions
+            ),
+        )
+        self.register_buffer("masked_bias", torch.tensor(-1e9))
+        self.rotary_emb = RotaryEmbedding(
+            self.rotary_ndims, config.max_position_embeddings, base=config.rotary_emb_base
+        )
+        self.norm_factor = torch.sqrt(torch.tensor(self.head_size, dtype=torch.float32)).to(torch.get_default_dtype())
+        #self.query_key_value = nn.Linear(config.hidden_size, 3 * config.hidden_size)
+        #self.dense = nn.Linear(config.hidden_size, config.hidden_size)
+
+    def forward(
+        self,
+        qkv,
+        #hidden_states,
+        attention_mask,
+        q_quant, k_quant, qk_quant, v_quant, requant,
+        head_mask=None,
+        layer_past=None,
+        use_cache=False,
+        output_attentions=False,
+    ):
+        has_layer_past = layer_past is not None
+
+        # Compute QKV
+        # Attention heads [batch, seq_len, hidden_size]
+        #   --> [batch, seq_len, (np * 3 * head_size)]
+        # qkv = self.query_key_value(hidden_states)
+
+        # [batch, seq_len, (num_heads * 3 * head_size)]
+        #   --> [batch, seq_len, num_heads, 3 * head_size]
+        new_qkv_shape = qkv.size()[:-1] + (self.num_attention_heads, 3 * self.head_size)
+        qkv = qkv.view(*new_qkv_shape)
+
+        # [batch, seq_len, num_attention_heads, 3 * head_size] --> 3 [batch, num_attention_heads, seq_len, head_size]
+        query = qkv[..., : self.head_size].permute(0, 2, 1, 3)
+        key = qkv[..., self.head_size : 2 * self.head_size].permute(0, 2, 1, 3)
+        value = qkv[..., 2 * self.head_size :].permute(0, 2, 1, 3)
+
+        # Compute rotary embeddings on rotary_ndims
+        query_rot = query[..., : self.rotary_ndims]
+        query_pass = query[..., self.rotary_ndims :]
+        key_rot = key[..., : self.rotary_ndims]
+        key_pass = key[..., self.rotary_ndims :]
+
+        # Compute token offset for rotary embeddings (when decoding)
+        seq_len = key.shape[-2]
+        offset = 0
+        if has_layer_past:
+            offset = layer_past[0].shape[-2]
+            seq_len += offset
+        cos, sin = self.rotary_emb(value, seq_len=seq_len)
+        query, key = apply_rotary_pos_emb(query_rot, key_rot, cos, sin, offset=offset)
+        query = torch.cat((query, query_pass), dim=-1)
+        key = torch.cat((key, key_pass), dim=-1)
+
+        # Cache QKV values
+        if has_layer_past:
+            past_key = layer_past[0]
+            past_value = layer_past[1]
+            key = torch.cat((past_key, key), dim=-2)
+            value = torch.cat((past_value, value), dim=-2)
+        present = (key, value) if use_cache else None
+
+        # quant
+        query = (query * q_quant).round().clamp(-128, 127).to(torch.float32)
+        key = (key * k_quant).round().clamp(-128, 127).to(torch.float32)
+        value = (value * v_quant).round().clamp(-128, 127).to(torch.float32)
+        # Compute attention
+        attn_output, attn_weights = self._attn(query, key, value, attention_mask, head_mask, q_quant, k_quant, qk_quant, v_quant)
+        attn_output = (attn_output * requant).round().clamp(-128, 127).to(torch.float32)
+
+        # Reshape outputs
+        attn_output = self._merge_heads(attn_output, self.num_attention_heads, self.head_size)
+        #attn_output = self.dense(attn_output)
+
+        outputs = (attn_output, present)
+
+        return outputs
+
+    @classmethod
+    def _split_heads(cls, tensor, num_attention_heads, attn_head_size):
+        """
+        Splits hidden dim into attn_head_size and num_attention_heads
+        """
+        # tensor: [bs, seq_len, hidden_size]
+        new_shape = tensor.size()[:-1] + (num_attention_heads, attn_head_size)
+        # -> [bs, seq_len, num_attention_heads, attn_head_size]
+        tensor = tensor.view(new_shape)
+        # -> [bs, num_attention_heads, seq_len, attn_head_size]
+        tensor = tensor.permute(0, 2, 1, 3)
+        return tensor
+
+    @classmethod
+    def _merge_heads(cls, tensor, num_attention_heads, attn_head_size):
+        """
+        Merges attn_head_size dim and num_attn_heads dim into hidden dim
+        """
+        # tensor [bs, num_attention_heads, seq_len, attn_head_size]
+        tensor = tensor.permute(0, 2, 1, 3).contiguous()
+        # -> [bs, seq_len, num_attention_heads, attn_head_size]
+        tensor = tensor.view(tensor.size(0), tensor.size(1), num_attention_heads * attn_head_size)
+        # -> [bs, seq_len, hidden_size]
+        return tensor
+
+    def _attn(self, query, key, value, attention_mask, head_mask, q_quant, k_quant, qk_quant, v_quant):
+        # q, k, v: [bs, num_attention_heads, seq_len, attn_head_size]
+        # compute causal mask from causal mask buffer
+        batch_size, num_attention_heads, query_length, attn_head_size = query.size()
+        key_length = key.size(-2)
+
+        causal_mask = self.bias[:, :, key_length - query_length : key_length, :key_length]
+
+        query = query.view(batch_size * num_attention_heads, query_length, attn_head_size)
+        key = key.view(batch_size * num_attention_heads, key_length, attn_head_size)
+        attn_scores = torch.zeros(
+            batch_size * num_attention_heads,
+            query_length,
+            key_length,
+            dtype=query.dtype,
+            device=key.device,
+        )
+        attn_scores = torch.baddbmm(
+            attn_scores,
+            query,
+            key.transpose(1, 2),
+            beta=1.0,
+            alpha=(torch.tensor(1.0, dtype=self.norm_factor.dtype, device=self.norm_factor.device) / self.norm_factor * (1 / q_quant) * (1 / k_quant)),
+        )
+        attn_scores = attn_scores.view(batch_size, num_attention_heads, query_length, key_length)
+
+        mask_value = torch.finfo(attn_scores.dtype).min
+        # Need to be a tensor, otherwise we get error: `RuntimeError: expected scalar type float but found double`.
+        # Need to be on the same device, otherwise `RuntimeError: ..., x and y to be on the same device`
+        mask_value = torch.tensor(mask_value, dtype=attn_scores.dtype).to(attn_scores.device)
+        attn_scores = torch.where(causal_mask, attn_scores, mask_value)
+
+        if attention_mask is not None:
+            # Apply the attention mask
+            attn_scores = attn_scores + attention_mask
+
+        attn_weights = nn.functional.softmax(attn_scores, dim=-1)
+        attn_weights = attn_weights.to(value.dtype)
+
+        # Mask heads if we want to
+        if head_mask is not None:
+            attn_weights = attn_weights * head_mask
+        
+        attn_weights = (attn_weights * qk_quant).round().clamp(0, 255).to(torch.uint8).to(torch.float32)
+        attn_output = torch.matmul(attn_weights, value)
+        attn_output = attn_output * ((1 / qk_quant) * (1 / v_quant))
+        return attn_output, attn_weights
+
+def gen_ref_gpt_int8(qkvs, beam_idxs, attn_masks, q_quant, k_quant, qk_quant, v_quant, requant):
+    class FakeConfig:
+        def __init__(self):
+            self.num_attention_heads = HEAD_NUM
+            self.hidden_size = HIDDEN_SIZE
+            self.rotary_pct = ROTARY_PCT
+            self.max_position_embeddings = MAX_POSITION_EMBEDDINGS
+            self.rotary_emb_base = ROTARY_EMB_BASE
+    config = FakeConfig()
+    net = GPTNeoXAttentionInt8(config)
+    seq_offset = 0
+    results = []
+    present = None
+    for (i, qkv_) in enumerate(qkvs):
+        if present:
+            layer_past = [p.index_select(0, torch.tensor(beam_idxs[i])) for p in present]
+        else:
+            layer_past = None
+        qkv = torch.from_numpy(qkv_).to(torch.bfloat16)
+        mask = torch.tensor(attn_masks[i][:,:qkv.shape[1]])
+        mask = mask[:, None,:, None]
+        attention_mask = (1.0 - mask) * torch.finfo(qkv.dtype).min
+        attn_output, present = net.forward(
+            qkv,
+            attention_mask,
+            q_quant, k_quant, qk_quant, v_quant, requant,
+            layer_past=layer_past,
+            use_cache=True,
+        )
+        seq_offset += qkv.shape[1]
+        results.append(attn_output.clone().to(dtype=torch.int8).detach().numpy())
+    return results
+
 # test matmul with s8 input and whole output type is s8
-def gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, is_ref=False):
+def gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, q_quant=0.0, k_quant=0.0, qk_quant=0.0, v_quant=0.0, requant=0.0):
     def make_model():
         # [batch, tokens, 32*80*3]
         qkv = opset.parameter([-1, -1, HEAD_NUM * SIZE_PER_HEAD * 3], Type.f32, name='qkv')
@@ -315,26 +514,13 @@ def gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, is_ref=False):
         attn_mask = opset.parameter([-1, MAX_SEQ_LEN], Type.i64, name='attn_mask')
 
         # custom op
-        requant = 10.0
-        if not is_ref:
-            q_quant, k_quant, qk_quant, v_quant = 127.0, 127.0, 255.0, 127.0
-            attn_output = opset.gpt_neox_attn(qkv, past_keys_num, beam_idx, attn_mask,
-                    layer_num=0, head_num=HEAD_NUM, size_per_head=SIZE_PER_HEAD, hidden_size=HIDDEN_SIZE, max_position_embeddings=MAX_POSITION_EMBEDDINGS,
-                    rotary_emb_base=ROTARY_EMB_BASE, rotary_pct=ROTARY_PCT, max_seq_len=MAX_SEQ_LEN,
-                    q_quant=q_quant, k_quant=k_quant, qk_quant=qk_quant, v_quant=v_quant,
-                    name=f'/model/gpt_neox/layers.0/attention/attn')
-            fq = opset.fake_quantize(attn_output, -127/requant, 127/requant, -127/requant, 127/requant, 255, "NUMPY", name=f'fq_input_0')
-            fq_result = opset.convert(fq, np.int8)
-        else:
-            q_quant, k_quant, qk_quant, v_quant = 0.0, 0.0, 0.0, 0.0
-            attn_output = opset.gpt_neox_attn(qkv, past_keys_num, beam_idx, attn_mask,
-                    layer_num=0, head_num=HEAD_NUM, size_per_head=SIZE_PER_HEAD, hidden_size=HIDDEN_SIZE, max_position_embeddings=MAX_POSITION_EMBEDDINGS,
-                    rotary_emb_base=ROTARY_EMB_BASE, rotary_pct=ROTARY_PCT, max_seq_len=MAX_SEQ_LEN,
-                    q_quant=q_quant, k_quant=k_quant, qk_quant=qk_quant, v_quant=v_quant,
-                    name=f'/model/gpt_neox/layers.0/attention/attn')
-            #attn_output = opset.clamp(attn_output, -12.7, 12.7, name='clamp1')
-            fq_result = opset.multiply(attn_output, np.array([requant], dtype=np.float32))
-            #fq_result = opset.fake_quantize(attn_output, -127, 127, -127, 127, 255, "NUMPY", name=f'fq_input_0')
+        attn_output = opset.gpt_neox_attn(qkv, past_keys_num, beam_idx, attn_mask,
+                layer_num=0, head_num=HEAD_NUM, size_per_head=SIZE_PER_HEAD, hidden_size=HIDDEN_SIZE, max_position_embeddings=MAX_POSITION_EMBEDDINGS,
+                rotary_emb_base=ROTARY_EMB_BASE, rotary_pct=ROTARY_PCT, max_seq_len=MAX_SEQ_LEN,
+                q_quant=q_quant, k_quant=k_quant, qk_quant=qk_quant, v_quant=v_quant,
+                name=f'/model/gpt_neox/layers.0/attention/attn')
+        fq = opset.fake_quantize(attn_output, -127/requant, 127/requant, -127/requant, 127/requant, 255, "NUMPY", name=f'fq_input_0')
+        fq_result = opset.convert(fq, np.int8)
 
         return Model([fq_result], [qkv, past_keys_num, beam_idx, attn_mask])
     net = make_model()
@@ -361,7 +547,7 @@ def gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, is_ref=False):
         req.set_input_tensors(inputs)
         req.infer()
         r, = req.outputs
-        results.append((np.array(r.data)+0.5).astype(np.int8))
+        results.append((np.array(r.data)).astype(np.int8))
     return results
 
 def test_gpt_bf16():
@@ -395,21 +581,39 @@ def test_gpt_bf16():
 
 def test_gpt_i8():
     print('testing i8...')
-    qkvs = [
-            np.random.randint(low=-1, high=2, size=[2, 100, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            np.random.randint(low=-1, high=2, size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            np.random.randint(low=-1, high=2, size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            np.random.randint(low=-1, high=2, size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            # np.random.random(size=[2, 100, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            # np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            # np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            # np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD * 3]).astype(np.float32),
-            ]
-    for i in range(len(qkvs)):
-        pass
-        # qkvs[i][:,:,0::3] = -1
-        # qkvs[i][:,:,1::3] = +1
-        # qkvs[i][:,:,2::3] = 0
+    low = -4
+    high = 4
+    range_ = high - low
+    q_quant = 127.0 / high
+    q = [
+            np.random.random(size=[2, 900, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+        ]
+    low = -2
+    high = 2
+    range_ = high - low
+    k_quant = 127.0 / high
+    k = [
+            np.random.random(size=[2, 900, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+        ]
+    low = -8
+    high = 8
+    range_ = high - low
+    v_quant = 127.0 / high
+    v = [
+            np.random.random(size=[2, 900, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+            np.random.random(size=[2, 1, HEAD_NUM * SIZE_PER_HEAD]).astype(np.float32)*range_+low,
+        ]
+    qkvs = []
+    for i in range(len(q)):
+        qkvs.append(np.concatenate([q[i], k[i], v[i]], axis=2))
     beam_idxs = [[-1, -1],
                  [1, 0],
                  [1, 1],
@@ -420,11 +624,10 @@ def test_gpt_i8():
         #np.save(f'xx{i}.npy', qkvs[i], allow_pickle=True)
         #qkvs[i] = np.load(f'xx{i}.npy', allow_pickle=True)
         pass
-    ref_results1 = gen_ref_gpt(qkvs, beam_idxs, attn_masks)
-    ref_results = gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, True)
-    ov_results = gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, False)
+    qk_quant, requant = 255.0, 10.0
+    ref_results = gen_ref_gpt_int8(qkvs, beam_idxs, attn_masks, q_quant, k_quant, qk_quant, v_quant, requant)
+    ov_results = gen_ov_gpt_i8(qkvs, beam_idxs, attn_masks, q_quant, k_quant, qk_quant, v_quant, requant)
     for (i, ref) in enumerate(ref_results):
-        #if not np.allclose(ref, ov_results[i], rtol=0.001, atol=0.01):
         if (np.abs(ref_results[i]- ov_results[i])>2).any():
             print(f'error: idx {i} not close')
     else:
